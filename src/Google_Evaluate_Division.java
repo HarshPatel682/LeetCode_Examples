733c721b302e5b853af575170acdb7dd9cb4a867
HarshPatel682 <patel682@purdue.edu>
2018-10-31 15:51:11 -0400

    added functions

--- src/Google_Evaluate_Division.java
+++ src/Google_Evaluate_Division.java
@@ -1,3 +1,19 @@
+/*
+* https://leetcode.com/problems/evaluate-division/description/
+*
+* This is a medium level coding example for Google
+* */
+
+import java.util.*;
+
 
 public class Google_Evaluate_Division {
+
+    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
