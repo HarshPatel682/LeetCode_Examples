7b8427e9eee4765ac516ae2fc0ef178f52cd93b3
HarshPatel682 <patel682@purdue.edu>
2018-10-31 16:10:45 -0400

    started the eval div

--- src/Google_Evaluate_Division.java
+++ src/Google_Evaluate_Division.java
@@ -9,11 +9,31 @@ import java.util.*;
 
 public class Google_Evaluate_Division {
 
-    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+    private static double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+
+        Map<String, List<String>> neighbors = new HashMap<>();
+        Map<String, List<Double>> weights = new HashMap<>();
+
+        double[] results = new double[queries.length];
+
+        for (int i = 0; i < equations.length; i++) {
+            if (!neighbors.containsKey(equations[i][0])) {
+                //if key not in the neighbors map, then initialize new array lists for both neighbors and weights
+                neighbors.put(equations[i][0], new ArrayList<String>());
+                weights.put(equations[i][0], new ArrayList<Double>());
+            }
+        }
+
         return null;
     }
 
     public static void main(String[] args) {
+        String[][] equations = { {"a", "b"} , {"b", "c"}};
+        double[] values = {2.0, 3.0};
+        String[][] queries = { {"a", "c"} , {"b", "a"}, {"a", "e"},{"a", "a"},{"x", "x"} };
 
+        double[] result = calcEquation(equations, values, queries);
+
+        System.out.println(result.toString());
     }
 }
